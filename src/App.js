078176425312afd9f802e84479adb9fb45a97351
Patel Mihir <patelmihir0430@gmail.com>
2024-03-12 16:42:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,9 @@ import Weapons from "./Design/Weapons/Weapons.js";
 function App() {
   return (
     <div>
-      {/* <Router>
-        <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/final-valo" element={<Home/>}/>
-        <Route path="/final-valo/Home" element={<Home/>}/>
-        <Route path="/final-valo/Weapons" element={<Weapons/>}/>
-        <Route path="/final-valo/Download" element={<Download/>}/>
-        </Routes>
-      </Router> */}
-    <Home/>
-    <Weapons/>  
-    <Download/>
+      <Home/>
+      <Download/>
+      <Weapons/>  
     </div>
   );
 }
