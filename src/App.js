03ea968ebc5ddf1e32c11de1d67b80378d78a975
Patel Mihir <patelmihir0430@gmail.com>
2024-03-12 15:44:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div>
       <Router>
         <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="" element={<Home/>}/>
         <Route path="home" element={<Home/>}/>
         <Route path="weapons" element={<Weapons/>}/>
         <Route path="download" element={<Download/>}/>
