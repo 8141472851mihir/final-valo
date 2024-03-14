3b0c14887880c33ba4ea16ad1a5b6b3f653298f2
Keyush Prajapati <keyushkarm@gmail.com>
2024-03-14 15:54:09 +0530

    chages 14-3-24 3:55PM
    fixed carousal width height
    button component
    card component
    card hover effect
    
    pages: about,carousal,maps,patch,views
    
    improve code efficiency using components

--- src/Design/Home/About.js
+++ src/Design/Home/About.js
@@ -1,21 +1,22 @@
 import React from 'react';
 function About() {
-return(
-<>
+    return (
+        <>
+            <div id="about" class="container p-md-5 p-2 p-lg-3 my-5 text-light">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 font_secondary">
+                        <h1 class="text_color_danger font_primary my-2 my-md-4">About Us</h1>
+                        <p>Blend your style and experience on a global, competitive stage. You have 13 rounds to attack and defend your side using sharp gunplay and tactical abilities. And, with one life per-round, you'll need to think faster than your opponent if you want to survive. Take on foes across Competitive and Unranked modes as well as Deathmatch and Spike Rush.</p>
 
-    <div class="row align-items-center">
-        <div class="col-lg-6 font_secondary">
-        <h1 class="text_color_danger font_primary my-2 my-md-4">About Us</h1>
-            <p>Blend your style and experience on a global, competitive stage. You have 13 rounds to attack and defend your side using sharp gunplay and tactical abilities. And, with one life per-round, you'll need to think faster than your opponent if you want to survive. Take on foes across Competitive and Unranked modes as well as Deathmatch and Spike Rush.</p>
-            
-        </div>
-        <div class="col-lg-6 ">
-            <div className='ratio ratio-16x9 mt-md-4 mt-4'>
-                <iframe class="mx-auto w-100 h-100 "src="https://www.youtube.com/embed/CZdftlOD_jU?mute=1&loop=1&controls=0" allowfullscreen></iframe>
+                    </div>
+                    <div class="col-lg-6 ">
+                        <div className='ratio ratio-16x9 mt-md-4 mt-4'>
+                            <iframe class="mx-auto w-100 h-100 " src="https://www.youtube.com/embed/CZdftlOD_jU?mute=1&loop=1&controls=0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</>
-)
+        </>
+    )
 }
 export default About;
--- src/Design/Home/Maps.js
+++ src/Design/Home/Maps.js
@@ -1,60 +1,26 @@
 import React from 'react';
+import Card from '../../components/Card';
 function Maps() {
-return(
-<>
-    <div class="col-md-3 my-3 my-sm-4 abc " >
-        <div class="renew-card ">
-            <div class="renew-image">
-                <img src="https://cdn.oneesports.gg/cdn-data/2023/01/Valorant_Lotus_Episode6ActI_Map.jpg"/>
+    return (
+        <>
+            <div id="popular-maps" class="container my-5 text-light">
+                <div class="row mb-2">
+                    <h1 class="border-bottom text_color_danger font_primary">Popular Maps</h1>
+                </div>
+                <div class="row mt-5">
+                <Card imgSrc="https://cdn.oneesports.gg/cdn-data/2023/01/Valorant_Lotus_Episode6ActI_Map.jpg" title="Lotus" href="https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-6-07/" description="Each map is a playground to showcase your creative thinking. Purpose-built for team strategies.Each map is a playground to showcas" />
+
+                <Card imgSrc="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltee14888179d221f8/5ee7d51725b4740c330ba55d/Loading_Screen_Split_v2.jpg" title="Split" href="https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-6-05/" description="Each map is a playground to showcase your creative thinking. Purpose-built for team strategies.Each map is a playground to showcas" />
+
+                <Card imgSrc="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt0c118364c6320f60/62a289d3891af05acaff06b1/Pearl_Gallery_01.jpg?auto=webp&width=915" title="Pearl" href="https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-6-05/" description="Each map is a playground to showcase your creative thinking. Purpose-built for team strategies.Each map is a playground to showcas" />
+
+                <Card imgSrc="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt1a720126e3713bba/6131bf518e16ab655b34901a/Fracture_Screenshot-8.jpg?auto=webp&width=915" title="Fracture" href="https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-6-05/" description="Each map is a playground to showcase your creative thinking. Purpose-built for team strategies.Each map is a playground to showcas" />
+
+                </div>
+
             </div>
-            <div class="renew--content">
-                <h3 class="renew-title font_primary ">Lotus</h3>
-                <p class="renew-description">
-                </p>
-                <a href="https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-6-05/" target="_blank"><button class="btn button-49 rounded-0 font_secondary ">View</button></a>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3 my-3 my-sm-4 abc" >
-        <div class="renew-card">
-            <div class="renew-image">
-                <img src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltee14888179d221f8/5ee7d51725b4740c330ba55d/Loading_Screen_Split_v2.jpg"/>
-            </div>
-            <div class="renew--content">
-                <h3 class="renew-title font_primary">Split</h3>
-                <p class="renew-description">
-                </p>
-                <a href="https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-6-05/" target="_blank"><button class="btn button-49 rounded-0 font_secondary">View</button></a>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3 my-3 my-sm-4 abc" >
-        <div class="renew-card">
-            <div class="renew-image">
-                <img src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt0c118364c6320f60/62a289d3891af05acaff06b1/Pearl_Gallery_01.jpg?auto=webp&width=915"/>
-            </div>
-            <div class="renew--content">
-                <h3 class="renew-title font_primary">Pearl</h3>
-                <p class="renew-description">
-                </p>
-                <a href="https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-6-05/" target="_blank"><button class="btn button-49 rounded-0 font_secondary">View</button></a>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3 my-3 my-sm-4 abc" >
-        <div class="renew-card">
-            <div class="renew-image">
-                <img src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt1a720126e3713bba/6131bf518e16ab655b34901a/Fracture_Screenshot-8.jpg?auto=webp&width=915"/>
-            </div>
-            <div class="renew--content">
-                <h3 class="renew-title font_primary">Fracture</h3>
-                <p class="renew-description">
-                </p>
-                <a href="https://playvalorant.com/en-us/news/game-updates/valorant-patch-notes-6-05/" target="_blank"><button class="btn button-49 rounded-0 font_secondary">View</button></a>
-            </div>
-        </div>
-    </div>
-</>
-)
+
+        </>
+    )
 }
 export default Maps;
