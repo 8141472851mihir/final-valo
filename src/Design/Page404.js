fefbdb69294173adf4e46e45644c25be22f82f8e
Patel Mihir <patelmihir0430@gmail.com>
2024-03-19 11:49:59 +0530

    Update Page404.js

--- src/Design/Page404.js
+++ src/Design/Page404.js
@@ -7,11 +7,11 @@ const Page404 = () => {
   return (
     <div>
       <Header/>
-      <div className="container d-sm-flex justify-content-center border" style={{height:'80vh'}}>
-        <div className="my-auto text-right border ">
+      <div className="container d-sm-flex justify-content-center" style={{height:'80vh'}}>
+        <div className="my-auto text-right ">
         <h1 className="text-light mt-sm-0 mt-4" style={{fontSize:'30vh'}}>404</h1>
         </div>
-        <div className="my-auto mx-2 border">
+        <div className="my-auto mx-2">
         <h1 className="text-light" style={{fontSize:'10vh'}}>Page Not Found!</h1>
         <p className="text-light" style={{fontSize:'2vh'}}>The page You are looking for was moved,removed,renamed or might never existed.</p>
         <Link to="/final-valo/" ><button className="button-49" style={{width:'200px'}}>Back to Home Page</button></Link>
